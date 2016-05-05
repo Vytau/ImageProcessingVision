cc2d67158fe14463bf1220403eb950fba13cf2d4
Vytautas <Vytautas@ALCHEMIST>
2016-05-05 14:05:35 +0200

    Add project files.

---
+++ IPV_assignments/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace IPV_assignments
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.imageBox5 = new Emgu.CV.UI.ImageBox();
+            this.imageBox6 = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(300, 300);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(318, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(300, 300);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(624, 12);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(300, 300);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox3.TabIndex = 2;
+            this.imageBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(949, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(948, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.Location = new System.Drawing.Point(12, 381);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(300, 300);
+            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox4.TabIndex = 2;
+            this.imageBox4.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(949, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(949, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // imageBox5
+            // 
+            this.imageBox5.Location = new System.Drawing.Point(318, 381);
+            this.imageBox5.Name = "imageBox5";
+            this.imageBox5.Size = new System.Drawing.Size(300, 300);
+            this.imageBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox5.TabIndex = 2;
+            this.imageBox5.TabStop = false;
+            // 
+            // imageBox6
+            // 
+            this.imageBox6.Location = new System.Drawing.Point(624, 381);
+            this.imageBox6.Name = "imageBox6";
+            this.imageBox6.Size = new System.Drawing.Size(300, 300);
+            this.imageBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox6.TabIndex = 2;
+            this.imageBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(754, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 684);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 684);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "original";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(754, 684);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "original";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(949, 185);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Camera";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.cameraBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 717);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox6);
+            this.Controls.Add(this.imageBox5);
+            this.Controls.Add(this.imageBox4);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private Emgu.CV.UI.ImageBox imageBox5;
+        private Emgu.CV.UI.ImageBox imageBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ IPV_assignments/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace IPV_assignments
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture;        //takes images from camera as image frames
+        private bool _captureInProgress; // checks if capture is executing
+        private Image<Bgr, byte> _imageFrame = new Image<Bgr, byte>(@"lena.jpg");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            imageBox1.Image = _imageFrame;
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            _imageFrame = _capture.QueryFrame().ToImage<Bgr, byte>();   //line 1
+
+            imageBox1.Image = _imageFrame;                              //line 2
+        }
+
+        private void ProcessFrameA(object sender, EventArgs arg)
+        {
+            Image<Bgr, byte> tempCloneImage = _imageFrame.Clone();
+
+            for (int i = 50; i < 200; i++)
+            {
+                tempCloneImage.Data[200, i, 0] = 255;
+                tempCloneImage.Data[200, i, 1] = 255;
+                tempCloneImage.Data[200, i, 2] = 255;
+                //double line
+                tempCloneImage.Data[200, i + 1, 0] = 255;
+                tempCloneImage.Data[200, i + 1, 1] = 255;
+                tempCloneImage.Data[200, i + 1, 2] = 255;
+            }
+            for (int i = 50 + 75; i < 275; i++)
+            {
+                tempCloneImage[i, 125] = new Bgr(0, 0, 0);
+                //double line
+                tempCloneImage[i, 125 + 1] = new Bgr(0, 0, 0);
+            }
+
+            imageBox2.Image = tempCloneImage;
+        }
+
+        private void ProcessFrameB(object sender, EventArgs arg)
+        {
+            Image<Bgr, byte> tempCloneImage = _imageFrame.Clone();
+
+            for (int x = 0; x < tempCloneImage.Rows; x++)
+            {
+                for (int y = 0; y < tempCloneImage.Cols; y++)
+                {
+                    Bgr color = tempCloneImage[x, y];
+                    color.Green = color.Green * 0.114;
+                    color.Blue = color.Blue * 0.587;
+                    color.Red = color.Red * 0.299;
+                    tempCloneImage[x, y] = color;
+                }
+            }
+            imageBox3.Image = tempCloneImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Idle += ProcessFrameA;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Idle += ProcessFrameB;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //ToDo
+            //implement C part on this buttondasdas
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //ToDo
+            //implement D part on this button
+        }
+
+        private void cameraBtn_Click(object sender, EventArgs e)
+        {
+            #region if capture is not created, create it now
+            if (_capture == null)
+            {
+                try
+                {
+                    _capture = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    button5.Text = "Start!"; //
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    button5.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+    }
+}
