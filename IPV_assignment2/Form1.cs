c823be51892a620aef30212f6d739c917b2397ff
Vytau <vytautas.skinkys@gmail.com>
2016-05-30 13:52:51 +0200

    started with assignment2. Managed setings with emguCV and load image

--- IPV_assignment2/Form1.cs
+++ IPV_assignment2/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,17 @@ namespace IPV_assignment2
 {
     public partial class Form1 : Form
     {
+        private readonly Image<Bgr, byte> _imageFrame = new Image<Bgr, byte>(@"..\..\Resources\sudoku-original.jpg");
+
         public Form1()
         {
             InitializeComponent();
+            imageBox1.Image = _imageFrame;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
